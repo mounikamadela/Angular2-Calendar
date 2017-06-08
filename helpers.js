14c3f633ef2dbbec87534415ac3c711cdb9f2450
keerthi05 <keerthi187879@gmail.com>
2017-06-08 09:45:47 +0530

    updated

--- helpers.js
+++ helpers.js
@@ -1,20 +1,17 @@
 angular
   .module('mwl.calendar.docs')
   .factory('alert', function($uibModal) {
-
-    function show(action, event) {
-      return $uibModal.open({
-        controller: function() {
-          var vm = this;
-          vm.action = action;
-          vm.event = event;
-        },
-        controllerAs: 'vm'
-      });
-    }
-
-    return {
-      show: show
-    };
-
-  });
+  function show(action, event) {
+    return $uibModal.open({
+      controller: function() {
+        var vm = this;
+        vm.action = action;
+        vm.event = event;
+      },
+      controllerAs: 'vm'
+    });
+  }
+  return {
+    show: show
+  };
+});
